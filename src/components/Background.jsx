c2184c2c25709dd6c4fd443080ede6a5a0d927de
aris <arsenis_sto@yahoo.gr>
2024-04-08 20:45:25 +0300

    bg change

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 
 export default function Background({page}) {
   return (
-    <>
-    <Image src="/bgBrighter.webp" alt="bg" priority fill className="fixed top-0 left-0 -z-10 h-screen w-screen " />
+    <div className="pointer-events-none fixed top-0 left-0 -z-10 h-screen w-screen ">
+    <Image src="/bgV2.webp" unoptimized={true} alt="bg" priority fill className="invisible md:visible  fixed top-0 left-0 -z-10 h-screen w-screen " />
+    <Image src="/bgV2-mobile.webp" unoptimized={true} alt="bg" priority fill className="visible md:invisible fixed top-0 left-0 -z-10 h-screen w-screen " />
       {/* <div className=" -z-10 fixed top-full left-full -translate-x-1/2  -translate-y-1/2  bg-gradient-radial from-0% to-40% from-myOrange/5 to-myBlack/0 rounded-full   size-[2000px] "></div>
         <div className=" -z-10 fixed top-[60%] left-0 -translate-x-1/2 -translate-y-1/2   bg-gradient-radial from-0% to-40% from-myOrange/5 to-myBlack/0 rounded-full   size-[2000px] "></div> */}
       {/* <div className={` -z-10 fixed top-full left-full -translate-x-1/2  -translate-y-1/2  bg-gradient-radial from-0% to-40% ${page===0||page===3?'from-myBlue/4  0':'from-myOrange/5'} to-myBlack/0 rounded-full   size-[2000px] transition-colors duration-1000  `}></div>
       <div className={` -z-10 fixed top-[60%] left-0 -translate-x-1/2 -translate-y-1/2   bg-gradient-radial from-0% to-40% ${page===0||page===3?'from-myBlue/4  0':'from-myOrange/5'} to-myBlack/0 rounded-full   size-[2000px] transition-colors duration-1000  `}></div> */}
-    </>
+    </div>
   );
 }
