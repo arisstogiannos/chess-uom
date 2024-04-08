af0371ea184bfa3de296100fb1a4dc13ac7e150e
aris <arsenis_sto@yahoo.gr>
2024-04-08 20:23:17 +0300

    bg-brighter

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Background({page}) {
   return (
     <>
-    <img src="/bg.webp" alt="bg" className="fixed top-0 left-0 -z-10 h-screen w-screen " />
+    <img src="/bgBrighter.webp" alt="bg" className="fixed top-0 left-0 -z-10 h-screen w-screen " />
       {/* <div className=" -z-10 fixed top-full left-full -translate-x-1/2  -translate-y-1/2  bg-gradient-radial from-0% to-40% from-myOrange/5 to-myBlack/0 rounded-full   size-[2000px] "></div>
         <div className=" -z-10 fixed top-[60%] left-0 -translate-x-1/2 -translate-y-1/2   bg-gradient-radial from-0% to-40% from-myOrange/5 to-myBlack/0 rounded-full   size-[2000px] "></div> */}
       {/* <div className={` -z-10 fixed top-full left-full -translate-x-1/2  -translate-y-1/2  bg-gradient-radial from-0% to-40% ${page===0||page===3?'from-myBlue/20':'from-myOrange/5'} to-myBlack/0 rounded-full   size-[2000px] transition-colors duration-1000  `}></div>
